2d172344dfe345acdb2b4e3db74d41d417ac2ef7
JorgeRomojaro11 <jromodel@myuax.com>
2023-11-28 11:56:06 +0100

    .

--- metodo_to_string.cpp
+++ metodo_to_string.cpp
@@ -4,3 +4,15 @@
 
 class Variant {
 public:
+    using Value = std::variant<int, double, char, std::string, std::vector<Variant>, void (*)()>;
+
+    Variant(const Value& value) : data(value) {}
+
+    std::string to_string() const {
+        struct Visitor {
+            std::string operator()(int value) const { return std::to_string(value); }
+            std::string operator()(double value) const { return std::to_string(value); }
+            std::string operator()(char value) const { return std::string(1, value); }
+            std::string operator()(const std::string& value) const { return value; }
+            std::string operator()(const std::vector<Variant>& value) const {
+                std::string result = "[";

c3305ce8ef2f25b69d36b52909fd6eb5cafd9da8
JorgeRomojaro11 <jromodel@myuax.com>
2023-11-28 12:09:56 +0100

    .

--- metodo_to_json_string.cpp
+++ metodo_to_json_string.cpp
@@ -7,4 +7,18 @@ class Variant {
 public:
     using Value = std::variant<int, double, char, std::string, std::vector<Variant>, std::function<void()>>;
     using Json = nlohmann::json;
+    Variant(const Value& value) : data(value) {}
+
+    Json to_json_string() const {
+        struct Visitor {
+            Json operator()(int value) const { return value; }
+            Json operator()(double value) const { return value; }
+            Json operator()(char value) const { return std::string(1, value); }
+            Json operator()(const std::string& value) const { return value; }
+            Json operator()(const std::vector<Variant>& value) const {
+                Json array = Json::array();
+                for (const auto& element : value) {
+                    array.push_back(element.to_json_string());
+                }
+                return array;
 
